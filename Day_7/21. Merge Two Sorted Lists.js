85c9fef6e2b6f19e10a8029e3f5808690200d81e
Sultan-99s <93755400+Sultan-99s@users.noreply.github.com>
2023-03-29 23:46:47 +0600

    Create 21. Merge Two Sorted Lists.js

---
+++ Day_7/21. Merge Two Sorted Lists.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let list = new ListNode(-1);
+    let temp = list;
+
+    if(!list1) return list2;
+    if(!list2) return list1;
+
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            temp.next = list1;
+            list1 = list1.next;
+        }
+        else{
+            temp.next = list2;
+            list2 = list2.next;
+        }
+        temp = temp.next;
+    }
+    if(list1) temp.next = list1;
+    if(list2) temp.next = list2;
+
+    return list.next;
+
+};
